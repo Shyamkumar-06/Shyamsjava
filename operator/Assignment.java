c6850266354186dd8433839666539f8b8c2f8a27
SHYAMKUMAR S <shyamkumar812006s@gmail.com>
2025-12-22 14:26:46 +0530

    add file

---
+++ operator/Assignment.java
@@ -0,0 +1,17 @@
+class Assignment
+{
+	public static void main(String[] args)
+	{
+		int n=10;
+		System.out.println("n:"+n);
+		n+=5;
+		System.out.println("n+=5:"+n);
+		n-=3;
+		System.out.println("n-=3:"+n);
+		n*=2;
+		System.out.println("n*=3:"+n);
+		n/=4;
+		System.out.println("n/=4:"+n);
+		
+	}
+}
---
+++ operator/Increment.java
@@ -0,0 +1,12 @@
+class Increment
+{
+	public static void main(String[] args)
+	{
+		int a=5;
+		System.out.println("a:"+a);
+		System.out.println("a++:"+(a++));
+		System.out.println("++a:"+(++a));
+		System.out.println("a--:"+(a--));
+		System.out.println("--a:"+(--a));
+	}
+}
