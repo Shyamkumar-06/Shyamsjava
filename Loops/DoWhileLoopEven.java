38f018bf9595b066e9175d16e3d94aae381f190c
SHYAMKUMAR S <shyamkumar812006s@gmail.com>
2025-12-30 22:25:06 +0530

    loop 30/12/2025

---
+++ Loops/DoWhileLoopEven.java
@@ -0,0 +1,16 @@
+class DoWhileLoopEven
+{
+	public static void main(String[] args)
+	{
+		int num=1;
+		do
+		{
+			if(num%2==0){
+			System.out.println("even number  :"+num);
+			}
+			num++;
+		}while(num<=30);
+		
+		
+	}	
+}
---
+++ Loops/WhileLoop.java
@@ -0,0 +1,14 @@
+class WhileLoop
+{
+	public static void main(String[] args)
+	{
+		int num=1;
+		while(num<=15)
+		{
+			System.out.println(num);
+			num++;
+		}
+		
+		
+	}	
+}
